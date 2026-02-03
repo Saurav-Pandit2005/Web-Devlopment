188d77413fbba1ced0b38fdb8fc51c58195871b8
Saurav Pandit <sauravpandit9691@gmail.com>
2026-02-03 18:09:07 +0530

    some changes..

--- Backend/Rest_Class/index.js
+++ Backend/Rest_Class/index.js
@@ -10,46 +10,52 @@ app.set("views", path.join(__dirname, "views"));
 
 app.use(express.static(path.join(__dirname, "public")));
 
-let posts = [
-  {
-    id: "1a",
-    username: "Saurav",
-    content: "I love coding..",
-  },
-  {
-    id: "2b",
-    username: "ShraddhaKhapra",
-    content: "Hardwork is important to achieve success..",
-  },
-  {
-    id: "3c",
-    username: "Gautam",
-    content: "I got my first internship..",
-  },
-];
+// let posts = [
+//   {
+//     id: "1a",
+//     username: "Saurav",
+//     content: "I love coding..",
+//   },
+//   {
+//     id: "2b",
+//     username: "ShraddhaKhapra",
+//     content: "Hardwork is important to achieve success..",
+//   },
+//   {
+//     id: "3c",
+//     username: "Gautam",
+//     content: "I got my first internship..",
+//   },
+// ];
 
-app.get("/posts", (req, res) => {
-  res.render("index.ejs", { posts });
-});
+// app.get("/posts", (req, res) => {
+//   res.render("index.ejs", { posts });
+// });
 
-app.get("/posts/new", (req, res) => {
-    res.render("new.ejs");
-} )
+// app.get("/posts/new", (req, res) => {
+//     res.render("new.ejs");
+// } )
 
-app.post("/posts", (req, res) => {
-    let {username, content} = req.body;
-    posts.push({username, content});
-    // res.send("post request working..")
-    res.redirect("/posts");
-})
+// app.post("/posts", (req, res) => {
+//     let {username, content} = req.body;
+//     posts.push({username, content});
+//     // res.send("post request working..")
+//     res.redirect("/posts");
+// })
 
-app.get("/posts/:id", (req, res) => {
-    let {id} = req.params;
-    let post  = posts.find((p) => id === p.id);
-    console.log(post);
-    res.render("show.ejs");
+// app.get("/posts/:id", (req, res) => {
+//     let {id} = req.params;
+//     let post  = posts.find((p) => id === p.id);
+//     console.log(post);
+//     res.render("show.ejs");
+// })
+
+app.get("/", (req, res) => {
+  res.send("serving working well!!..");
 })
 
 app.listen(port, () => {
   console.log(`Listing on port ${port}`);
 });
+
+
